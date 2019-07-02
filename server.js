7e8f28bcf17ddc7f81aaf9eac5fb6b098960141a
ghosoun89 <goson.aldabea98@gmail.com>
2019-07-02 23:20:24 +0300

    create front end

--- server.js
+++ server.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const users = require('./roots/api/users');
-const profile = require('./roots/api/profile');
-const posts = require('./roots/api/posts');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require('./config/keys').mongoURI;
 
+// Connect to MongoDB
 mongoose
-.connect(db)
-.then(() =>console.log('mongoDB connnected'))
-.catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true }) // Let us remove that nasty deprecation warrning :)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.get('/',(req, res) => res.send('hello'));
+// Passport middleware
+app.use(passport.initialize());
 
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
